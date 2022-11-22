f7a5254ed6c11fdeca86429a1ca913a656cb2467
tzyan35 <zhongyan1997@hotmail.com>
2022-11-22 23:13:12 +0800

    everything

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -2,18 +2,42 @@ import Header from "./components/header/Header";
 // import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Login from "./pages/login/Login";
-
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
+  const user = false;
   return (
-    <>
-    <div className="App">
-   
-     <TopBar/>
-      {/* <Header/>  */}
-          {/* <Home/> */}
-      <Login/>
-    </div>
-    </>
+    <Router>
+        <TopBar />
+<Routes>
+  <Route strict exact path="/">
+    <Home/>
+  </Route>
+  <Route strict exact path="/register">
+   {user ? <Home/> : <Register/>} 
+  </Route>
+  <Route strict exact path="/login">
+  {user ? <Home/> : <Login/>} 
+  </Route>
+  <Route strict exact path="/write">
+  {user ? <Write/> : <Register/>} 
+  </Route>
+  <Route strict exact path="/settings">
+    <Settings/>
+  </Route>
+  <Route strict exact path="/post/:id">
+    <Single/>
+  </Route>
+</Routes>
+
+        <Login />
+ 
+        </Router>
   );
 }
 
---
+++ test-app/src/pages/register/Register.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./register.css";
+const Register = () => {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your username..."
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your email..."
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password"
+        />
+        <button className="registerButton">register</button>
+      </form>
+      <button className="registerLoginButton">Register</button>
+    </div>
+  );
+};
+
+export default Register;
